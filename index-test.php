0a9b03f4bb7cef92fda54d080f2ff69674e3e4e8
Vishal Panchal <vishal.panchal@bypt.in>
2018-02-12 13:41:00 +0530

    Initial Commit

---
+++ index-test.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
+
+use ByptPmt\Test;
+
+
+echo Test::task();
+echo "<br/>";
+
+
+?>
---
+++ src/ByptPmt/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vishal
+ * Date: 17-08-2017
+ * Time: 17:29
+ */
+
+namespace ByptPmt;
+
+
+class Test
+{
+    public static function task()
+    {
+        return 'Hello World, Composer!';
+    }
+}
